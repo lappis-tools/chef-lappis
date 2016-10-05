1ae36a8f692d9406028efb4c57624d30be8c367a
Paulo Tada <paulohtfs@gmail.com>
2016-10-05 17:00:36 -0300

    Fixing the postfix cookbook: removing master file

--- cookbooks/postfix/recipes/default.rb
+++ cookbooks/postfix/recipes/default.rb
@@ -6,7 +6,6 @@
 package 'postfix'
 service 'postfix'
 
-cookbook_file '/etc/postfix/main.cf'
-cookbook_file '/etc/postfix/master.cf' do
+cookbook_file '/etc/postfix/main.cf' do
   notifies :restart, 'service[postfix]'
 end
