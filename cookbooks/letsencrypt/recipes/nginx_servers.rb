50d62bf21e2b9014a1c616918be2f4843794287c
Gabriel Silva <gabriel93.silva@gmail.com>
2016-11-18 22:46:12 -0200

    Adds upstream to server blocks
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- cookbooks/letsencrypt/recipes/nginx_servers.rb
+++ cookbooks/letsencrypt/recipes/nginx_servers.rb
@@ -3,6 +3,7 @@ node['crt_domains'].each do | server, params |
 		template "/etc/nginx/sites-available/#{ server }-server" do
 			source 'server_conf.erb'
 			variables({
+				server:       server,
 				server_ip:    node['peers']["#{server}"] || node['peers']['letsencrypt'],
 				service_port: params['service_port'],
 				server_name:  params['server_name']
