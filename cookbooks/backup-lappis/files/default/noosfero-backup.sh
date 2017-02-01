2170cbdb8147e5aca02329d804fb26baa830f7ce
LAPPIS <lappis@services>
2017-02-01 16:25:21 +0000

    Removing unnecessary backup-lappis cookbook
    Signed-off-by: Luan Guimarães <livreluan@gmail.com>
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- cookbooks/backup-lappis/files/default/noosfero-backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BKP_DIR_NOOSFERO=/var/backups/portal
-
-#Apaga os backups com mais de 3 dias
-cd $BKP_DIR_NOOSFERO
-for i in `find $BKP_DIR_NOOSFERO/ -maxdepth 1 -type d -mtime +3 -print`; do rm -rf $i; done
-
