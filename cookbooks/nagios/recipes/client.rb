ce9ff1542a51c6ef2b229891a7c8c20a8a4f8aca
LAPPIS <lappis@services>
2017-04-17 13:52:05 +0000

    Create a file config to monitoring nagios client

--- cookbooks/nagios/recipes/client.rb
+++ cookbooks/nagios/recipes/client.rb
@@ -14,7 +14,7 @@ filesystem = `df -h / | grep '^/dev' | awk ' {print $1} '`
 filesystem = filesystem.gsub(/\//, "\\/")
 filesystem = filesystem.strip
 
-#TODO change /dev/vda ti filesystem variable
+#TODO change /dev/vda to filesystem variable
 execute 'change the filesystem in command[check_hda1]' do
 	command 'sed -i "s/\/dev\/hda1/%{filesystem}/g" /etc/nagios/nrpe.cfg' % {filesystem: filesystem}
 end
