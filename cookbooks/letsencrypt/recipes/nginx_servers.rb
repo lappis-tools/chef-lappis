22683ba4f060545c243c91666cd3ce70d1d64b51
Luan Guimarães <livreluan@gmail.com>
2017-04-25 12:54:00 -0300

    Creating new default configuration file to NGINX

--- cookbooks/letsencrypt/recipes/nginx_servers.rb
+++ cookbooks/letsencrypt/recipes/nginx_servers.rb
@@ -1,20 +1,26 @@
 node['crt_domains'].each do | server, params |
-  template "/etc/nginx/sites-available/#{ server }-server" do
-    source 'server_conf.erb'
-    variables({
-      server:       server,
-      server_ip:    node['peers']["#{server}"] || node['peers']['letsencrypt'],
-      service_port: params['service_port'],
-      server_name:  params['server_name']
-    })
-    mode 0644
-  end
+    template "/etc/nginx/sites-available/#{ server }-server" do
+      source( if server != 'default' then
+          'server_conf.erb'
+        else
+          'default_server_conf.erb'
+        end
+      )
 
-	if server != 'default'
+      variables({
+        server:       server,
+        server_ip:    node['peers']["#{server}"] || node['peers']['letsencrypt'],
+        service_port: params['service_port'],
+        server_name:  params['server_name']
+      })
+      mode 0644
+    end
+
+  if server != 'default'
     link "/etc/nginx/sites-enabled/#{server}-server" do
       to "/etc/nginx/sites-available/#{server}-server"
     end
-	end
+  end
 end
 
 service 'nginx' do
